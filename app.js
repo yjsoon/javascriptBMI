061ced9c3f9d7d13f12665d87640d1207fc1628a
YJ Soon <yjsoon@tinkertanker.com>
2015-10-20 12:02:37 +0800

    Refactored to jQuery

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
+$(function() {
+  //alert("jQuery loaded!");
+});
+
 var calculateBMI = function() {
-  var weightField = document.getElementById("weightField");
-  var weight = weightField.value;
-  var heightField = document.getElementById("heightField");
-  var height = heightField.value;
+  var weightField = $("#weightField");
+  var weight = weightField.val() * 1.0;
+  var heightField = $("#heightField");
+  var height = heightField.val() * 1.0;
 
   // PART 1: Calculate BMI and show in alert
   var bmi = weight / (height * height);
-  var resultDiv = document.getElementById("result");
-  resultDiv.innerHTML = "Your BMI is " + bmi;
-  var categoryDiv = document.getElementById("category");
+  var resultDiv = $("#result");
+  resultDiv.html("Your BMI is " + bmi);
+  var categoryDiv = $("#category");
 
   // PART 2: Check for health
   if (bmi < 18.5) {
-    categoryDiv.innerHTML = "You are underweight plz eat more";
+    categoryDiv.html("You are underweight plz eat more");
   } else if (bmi < 22.5) {
-    categoryDiv.innerHTML = "Very good plz don't get hit by a car";
+    categoryDiv.html("Very good plz don't get hit by a car");
   } else if (bmi < 27.5) {
-    categoryDiv.innerHTML = "Overweight";
+    categoryDiv.html("Overweight");
   } else {
-    categoryDiv.innerHTML = "Obese";
+    categoryDiv.html("Obese");
   }
 };
 
